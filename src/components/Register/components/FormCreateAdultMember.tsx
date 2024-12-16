52bc66e2778ba56710b22340d30e3430d10f6326
hungdd <z1zhungz1z@gmail.com>
2024-12-16 16:41:11 +0700

    ĐÂY LALANF 3

--- src/components/Register/components/FormCreateAdultMember.tsx
+++ src/components/Register/components/FormCreateAdultMember.tsx
@@ -12,7 +12,7 @@ import moment, { Moment } from "moment";
 import rules from "@src/scenes/Validation";
 
 
-export interface IRegisterProps {
+export interface IRegisterProps {ssSAs
 	onSuccessRegister: () => void;
 	onCancel?: () => void;
 }
